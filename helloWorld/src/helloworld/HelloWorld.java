8516f2a3a2baa80c8803d229adfeaed7adf9c54f
HarryStericker <hstericker@gmail.com>
2017-04-12 10:20:38 +0100

    Add files via upload

---
+++ helloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+/**
+ *
+ * @author Harry Stericker
+ * @version 1.0 
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
